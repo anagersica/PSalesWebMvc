70805b297add612583f7ee5f6c161b5c1e0e792f
unknown <anagersicahd@gmail.com>
2021-04-24 13:54:40 -0300

    CRUD Scaffolding

---
+++ PSalesWebMvc/Data/PSalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PSalesWebMvc.Models;
+
+namespace PSalesWebMvc.Data
+{
+    public class PSalesWebMvcContext : DbContext
+    {
+        public PSalesWebMvcContext (DbContextOptions<PSalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PSalesWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- PSalesWebMvc/Startup.cs
+++ PSalesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PSalesWebMvc.Data;
 
 namespace PSalesWebMvc
 {
@@ -33,6 +35,9 @@ namespace PSalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<PSalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PSalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
