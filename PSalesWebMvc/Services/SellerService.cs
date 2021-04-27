e0ea538ebfcd5c1c8fd5efde768450d6b3d3c362
Admin <anagersicahd@gmail.com>
2021-04-27 18:05:07 -0300

    Update seller and custom service exception

--- PSalesWebMvc/Services/SellerService.cs
+++ PSalesWebMvc/Services/SellerService.cs
@@ -6,6 +6,7 @@ using PSalesWebMvc.Models;
 using Microsoft.EntityFrameworkCore;
 using PSalesWebMvc.Data;
 using Microsoft.EntityFrameworkCore;
+using PSalesWebMvc.Services.Exceptions;
 
 //Serviços dentro de Model- contem operações/regra de negócio/atualizar/salvar/acessar dados referentes ao Seller
 namespace PSalesWebMvc.Services
@@ -18,27 +19,44 @@ namespace PSalesWebMvc.Services
 
             _context = context;
         }
-       public List<Seller> FindAll()
+        public List<Seller> FindAll()
         {
             return _context.Seller.ToList();//busca do BD todos os vendedores
 
         }
         public void Insert(Seller obj)//método inclui ação insert no botao create do form de novo funhcionário
         {
-           //tiramos o objeto pq inserimos um caixa de seleção( obj.Department = _context.Department.First();//preenche com o primeiro id de Depart a col DepartId de Seller
+            //tiramos o objeto pq inserimos um caixa de seleção( obj.Department = _context.Department.First();//preenche com o primeiro id de Depart a col DepartId de Seller
             _context.Add(obj);
             _context.SaveChanges();
         }
         //implementando para deletar um seller
-        public Seller FindById(int id )
+        public Seller FindById(int id)
         {
             return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);//sem o INCLUDE, por padrão carrega apenas dados da tabela SELLER 
         }
-        public void Remove (int id)
+        public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)//intercepta exceção de nível a dado e lança a nível de serviço:
+            {
+                throw new DbConcurrencyException(e.Message);//exceção a nível de serviço
+            }
+        }
     }
 }
+
